9a32600fc017f216a7c91f20feba49b6d0d19a27
Juanma Carmona <juanmacarmona97@gmail.com>
2024-01-11 12:21:28 +0100

    Ejercicio 2 terminado

--- Tema 7/T7Ejercicio2/app/src/main/java/com/example/t7ejercicio2/MainActivity.java
+++ Tema 7/T7Ejercicio2/app/src/main/java/com/example/t7ejercicio2/MainActivity.java
@@ -2,7 +2,13 @@ package com.example.t7ejercicio2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button definePrefs = findViewById(R.id.definePrefs);
+        Button cargarPrefs = findViewById(R.id.recuperaPrefs);
+
+        definePrefs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent preferencias = new Intent(MainActivity.this,PreferenciasActividad.class);
+                startActivity(preferencias);
+            }
+        });
+
+        cargarPrefs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(MainActivity.this);
+
+                Log.i("Preferencias","Opcion 1: "+prefs.getBoolean("unicoSO",false));
+                Log.i("Preferencias","Opcion 2: "+prefs.getString("elegirSO",""));
+                Log.i("Preferencias","Opcion 3: "+prefs.getString("elegirVersion",""));
+            }
+        });
     }
 }
