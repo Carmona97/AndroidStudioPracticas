25f1bffecb214e58f4f9023299be58ecbd7d1722
Juanma Carmona <juanmacarmona97@gmail.com>
2024-01-23 13:00:24 +0100

    actividad 3

--- Tema 7/T7Ejercicio3/app/src/main/java/com/example/t7ejercicio3/ActividadPreferencias.java
+++ Tema 7/T7Ejercicio3/app/src/main/java/com/example/t7ejercicio3/ActividadPreferencias.java
@@ -1,14 +1,16 @@
-package com.example.t7ejercicio2;
+package com.example.t7ejercicio3;
 
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
 
 import androidx.annotation.Nullable;
 
-public class PreferenciasActividad extends PreferenceActivity {
+public class ActividadPreferencias extends PreferenceActivity {
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.preferencias);
     }
+
 }
