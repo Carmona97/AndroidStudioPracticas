18408545710f54b213e6ee21ec1afbf790e02c1e
Juanma Carmona <juanmacarmona97@gmail.com>
2024-02-28 17:55:41 +0100

    Proyecto Examen 2 Tema 7

---
+++ Tema 7/CarmonaJuanManuelT7Examen2/app/src/main/java/com/example/carmonajuanmanuelt7examen2/PersonaDB.java
@@ -0,0 +1,30 @@
+package com.example.carmonajuanmanuelt7examen2;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class PersonaDB extends SQLiteOpenHelper {
+
+    String sqlCreate = "CREATE TABLE Personas(clave INTEGER PRIMARY KEY AUTOINCREMENT,nombre TEXT, telefono TEXT, avatar INTEGER)";
+    String sqlDelete = "DELETE FROM Personas";
+
+
+    public PersonaDB(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(sqlCreate);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int i, int i1) {
+
+        db.execSQL(sqlDelete);
+        db.execSQL(sqlCreate);
+    }
+}
